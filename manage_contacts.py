97c21088c63f29dd4fbb883035aeb8b7fa63e53c
Rebekah Heal <heal.rebekah@gmail.com>
2020-09-22 16:30:39 +1000

    created user login and create_user methods

--- manage_contacts.py
+++ manage_contacts.py
@@ -4,6 +4,7 @@ from contact import Contact
 from file import File
 
 class ManageContacts: #subclass of contact??
+
     @classmethod
     def email_validation(cls, answers, current):
         if not re.match('^[a-z0-9]+[\._-]?[a-z0-9]+[@]\w+[.]\w{2,3}[\.]?(\w{2,3})?$', current):
@@ -16,38 +17,31 @@ class ManageContacts: #subclass of contact??
         return True
 
     def add_contact(self, contacts_list: dict, groups_list: list) -> None:
-        contact_info = [inquirer.Text(name='firstname', message='New contact first name'), inquirer.Text(name='lastname', message='New contact last name'), inquirer.Text(name='email', message='{firstname} {lastname} email', validate=ManageContacts.email_validation), inquirer.Text(name='phone', message='{firstname} {lastname} phone number', validate=ManageContacts.phone_validation), inquirer.Checkbox(name='groups', message='Select groups for {firstname} {lastname}', choices=[group for group in groups_list], default=[])]
-
-        contact_info = inquirer.prompt(contact_info)
+        contact_info = inquirer.prompt([inquirer.Text(name='firstname', message='New contact first name'), inquirer.Text(name='lastname', message='New contact last name'), inquirer.Text(name='email', message='{firstname} {lastname} email', validate=ManageContacts.email_validation), inquirer.Text(name='phone', message='{firstname} {lastname} phone number', validate=ManageContacts.phone_validation), inquirer.Checkbox(name='groups', message='Select groups for {firstname} {lastname}', choices=[group for group in groups_list], default=[])])
 
         new_contact = Contact(**contact_info)
         new_contact.format_data(contacts_list)
 
     # classmethod??
     def select_contact(self, contacts_list: dict) -> dict:
-        choice = [inquirer.List('selected', message='Select Contact', choices=contacts_list.keys())]
-        choice = inquirer.prompt(choice)
+        choice = inquirer.prompt([inquirer.List('selected', message='Select Contact', choices=contacts_list.keys())])
         return contacts_list[choice['selected']]
 
-    # getter and setter method??
+    # get contact from select_contact function
     def edit_contact(self, contact: dict, contacts_list: dict) -> None:
-        edit = [inquirer.List('field', message='Choose field to edit', choices=['name', 'email', 'phone'])]
-        edit = inquirer.prompt(edit)
+        edit = inquirer.prompt([inquirer.List('field', message='Choose field to edit', choices=['name', 'email', 'phone'])])
 
         if edit['field'] == 'name':
-            edit = [inquirer.Text('firstname', message='Enter firstname'), inquirer.Text('lastname', message='Enter lastname')]
-            edit = inquirer.prompt(edit)
+            edit = inquirer.prompt([inquirer.Text('firstname', message='Enter firstname'), inquirer.Text('lastname', message='Enter lastname')])
             new_name = edit['firstname'].capitalize() + " " + edit['lastname'].capitalize()
             contacts_list[new_name] = contacts_list.pop(contact['name'])
             contacts_list[new_name]['name'] = new_name
         elif edit['field'] == 'email':
-            edit = [inquirer.Text('email', message='Enter email', validate=ManageContacts.email_validation)]
-            edit = inquirer.prompt(edit)
+            edit = inquirer.prompt([inquirer.Text('email', message='Enter email', validate=ManageContacts.email_validation)])
             new_data = edit['email']
             contacts_list[contact['name']]['email'] = edit['email']
         elif edit['field'] == 'phone':
-            edit = [inquirer.Text('phone', message='Enter phone number', validate=ManageContacts.phone_validation)]
-            edit = inquirer.prompt(edit)
+            edit = inquirer.prompt([inquirer.Text('phone', message='Enter phone number', validate=ManageContacts.phone_validation)])
             new_data = edit['phone']
             contacts_list[contact['name']]['phone'] = edit['phone']
 
@@ -55,18 +49,25 @@ class ManageContacts: #subclass of contact??
         print(f"Name: {contact['name']}\nEmail: {contact['email']}\nPhone: {contact['phone']}\nGroups: {contact['groups']}")
 
     def view_all_contacts(self, contacts_list):
-        print(f"{'Name:':20}{'Email:':32}{'Phone:':20}{'Groups:':20}")
+        print(f"{'Name:':25}{'Email:':35}{'Phone:':20}{'Groups:':20}")
         for contact, details in contacts_list.items():
-            print(f"{details['name']:20}{details['email']:32}{details['phone']:20}{details['groups']}")
+            print(f"{details['name']:25}{details['email']:35}{details['phone']:20}{details['groups']}")
 
 
 
 contact = ManageContacts()
-contacts_list = File.load_data('client.txt')
+# contacts_list = File.load_data('client.json')
 groups = ['family', 'friends', 'coworkers']
+load_file = File.load_data('client.json')
+# print(load_file)
+user = {'password': 'fool', 'contacts': {}}
+user['contacts'] = load_file
+print(user)
+contacts_list = user['contacts']
+
 # contact.add_contact(contacts_list, groups)
-# File.save_to_file('client.txt', contacts_list)
-# contact.select_contact(contacts_list)
+File.save_to_file('client.json', contacts_list)
+contact.select_contact(contacts_list)
 contact.edit_contact(contact.select_contact(contacts_list), contacts_list)
-# contact.view_all_contacts(contacts_list)
-print(contacts_list)
+contact.view_all_contacts(contacts_list)
+# print(contacts_list)
---
+++ user.py
@@ -0,0 +1,48 @@
+from file import File
+import inquirer
+
+class User:
+    user_options = ["Login", "Create User"]
+
+    @classmethod
+    def login(self, user_data):
+        def username_validation(answers, current):
+            if not user_data.get(current, True):
+                raise errors.ValidationError('', reason="Invalid username.")
+            return True
+        def password_validation(answers, current):
+            if current != user_data[answers['username']]['password']:
+                raise errors.ValidationError('', reason="Incorrect password.")
+            return True
+
+        user = inquirer.prompt([inquirer.Text('username', message="Enter username", validate=username_validation), inquirer.Password('password', message="Enter Password", validate=password_validation)])
+
+    @classmethod
+    def create_user(self, user_data):
+        def username_validation(answers, current):
+            if user_data.get(current, False):
+                raise errors.ValidationError('', reason="Username already in use.")
+            current = current.lower()
+            return True
+
+        def password_validation(answers, current):
+            if current != answers['initial_password']:
+                raise errors.ValidationError('', reason="Passwords do not match.")
+            return True
+
+        new_user = inquirer.prompt([inquirer.Text('username', message="Enter Username", validate=username_validation), inquirer.Password('initial_password', message="Enter Password"), inquirer.Password('validated_password', message="Re-Enter Password", validate=password_validation)])
+
+        user_data[new_user['username'].lower()] = {'password': new_user['validated_password'], 'contacts': {}}
+
+# new = User()
+user_data = File.load_data('client.json')
+# new.create_user(user_data)
+
+# user = User()
+options = inquirer.prompt([inquirer.List('choice', message="WELCOME TO THE APP!!!", choices=User.user_options)])
+if options['choice'] == 'Login':
+    user = User.login(user_data)
+else:
+    User.create_user(user_data)
+    File.save_to_file('client.json', user_data)
+    # options
