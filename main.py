bba120bb81ccd843cf3c960f3b2dca44fcc97872
Rebekah Heal <heal.rebekah@gmail.com>
2020-09-26 14:39:32 +1000

    updated yaml

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from user import User
 from file import File
 
-try:
-    file_path = 'client.json'
-    user_data = File.load_data(file_path)
-    current_user = User.user_menu(user_data, file_path)
+# try:
+file_path = 'client.json'
+user_data = File.load_data(file_path)
+current_user = User.user_menu(user_data, file_path)
 
-    contacts_dict = user_data[current_user]['contacts']
+contacts_dict = user_data[current_user]['contacts']
 
-    groups_list = user_data[current_user]['groups_dict'].keys()
-    groups_dict = user_data[current_user]['groups_dict']
+groups_list = user_data[current_user]['groups_dict'].keys()
+groups_dict = user_data[current_user]['groups_dict']
 
-    User.main_menu(user_data, contacts_dict, groups_dict, file_path,
-                   current_user)
-except:
-    print("\nSomething's gone terribly wrong! ", end="")
-    print("Feel free to contact *such and such* for assistance.")
+User.main_menu(user_data, contacts_dict, groups_dict, file_path, current_user)
+# except:
+#     print("\nSomething's gone terribly wrong! ", end="")
+#     print("Feel free to contact *such and such* for assistance.")
