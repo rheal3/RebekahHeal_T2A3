af07d7c4bc64d31c6e7caa0f8bf6b2626144c4dc
Rebekah Heal <heal.rebekah@gmail.com>
2020-09-26 10:18:21 +1000

    flake8 on main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from user import User
 from file import File
-from manage_contacts import ManageContacts
-import inquirer #take away later?
 
-# try:
-file_path = 'client.json'
-user_data = File.load_data(file_path)
-current_user = User.user_menu(user_data, file_path)
+try:
+    file_path = 'client.json'
+    user_data = File.load_data(file_path)
+    current_user = User.user_menu(user_data, file_path)
 
-contacts_dict = user_data[current_user]['contacts']
+    contacts_dict = user_data[current_user]['contacts']
 
-groups_list = user_data[current_user]['groups_dict'].keys() # import from groups function
-groups_dict = user_data[current_user]['groups_dict']
+    groups_list = user_data[current_user]['groups_dict'].keys()
+    groups_dict = user_data[current_user]['groups_dict']
 
-
-
-User.main_menu(user_data, contacts_dict, groups_dict, file_path, current_user)
-# except:
-#     print("\nSomething's gone terribly wrong! Feel free to contact *such and such* for assistance.")
+    User.main_menu(user_data, contacts_dict, groups_dict, file_path, current_user)
+except:
+    print("\nSomething's gone terribly wrong! Feel free to contact *such and such* for assistance.")
